3e55207b4d2136ffde7375a683d483d8fbd48cac
Zeldriskinng44 <144282751+Zeldriskinng44@users.noreply.github.com>
2025-02-16 17:33:43 +0100

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  discordToken: 'TOKEN', // توكن البوت
-  joinVoiceChannelId: 'ID', // ايدي الروم لصنع رومك
-  categoryId: 'ID', // ايدي الكاتوجري
+  discordToken: 'MTMxMTQ3NDI5NjU4MDg3MDIwNQ.G0nlwE.IOA_ig42vypc5TLs7j2g1vXRtjKnqh26hv8ydg', // توكن البوت
+  joinVoiceChannelId: '1339743023927656512', // ايدي الروم لصنع رومك
+  categoryId: '1339743023927656510', // ايدي الكاتوجري
   language: 'ar', // ar = للعربي | en = for english
 
   botPermissions: [
